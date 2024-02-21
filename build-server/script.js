533ae464712b06fd8e79c14432e7705715fffd7f
shrey kataria <shreykataria1@gmail.com>
2024-02-22 00:24:02 +0530

    fixing scriptjs

--- build-server/script.js
+++ build-server/script.js
@@ -15,43 +15,43 @@ const s3Client = new S3Client({
 const PROJECT_ID = process.env.PROJECT_ID;
 
 async function init() {
-  console.log("executing script.js");
-
+  console.log("Executing script.js");
   const outDirPath = path.join(__dirname, "output");
 
-  const p = exec(`cd ${outDirPath} && npm install && npm run build `);
+  const p = exec(`cd ${outDirPath} && npm install && npm run build`);
 
   p.stdout.on("data", function (data) {
     console.log(data.toString());
   });
 
   p.stdout.on("error", function (data) {
-    console.log("error", data.toString());
+    console.log("Error", data.toString());
   });
 
   p.on("close", async function () {
-    console.log("Build complete");
-    const distFolderPath = path.join(__dirname, "output , dist");
+    console.log("Build Complete");
+    const distFolderPath = path.join(__dirname, "output", "dist");
     const distFolderContents = fs.readdirSync(distFolderPath, {
       recursive: true,
     });
+
     for (const file of distFolderContents) {
       const filePath = path.join(distFolderPath, file);
       if (fs.lstatSync(filePath).isDirectory()) continue;
 
       console.log("uploading", filePath);
 
-      const putObjCommand = new PutObjectCommand({
+      const command = new PutObjectCommand({
         Bucket: "vercel-project-shrey",
-        Key: `__output/${PROJECT_ID}/${file}`,
+        Key: `__outputs/${PROJECT_ID}/${file}`,
         Body: fs.createReadStream(filePath),
         ContentType: mime.lookup(filePath),
       });
 
-      await s3Client.send(putObjCommand);
+      await s3Client.send(command);
       console.log("uploaded", filePath);
     }
-    console.log("done...");
+    console.log("Done...");
   });
 }
 
